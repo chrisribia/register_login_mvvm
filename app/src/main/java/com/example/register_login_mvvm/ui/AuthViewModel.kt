a79772aa2573e1f5753207d02f5220bd2b4b5f3c
chrisribia <chrisribia@gmail.com>
2019-07-23 14:37:33 +0300

    register xml file and its data binding with authmodel

--- app/src/main/java/com/example/register_login_mvvm/ui/AuthViewModel.kt
+++ app/src/main/java/com/example/register_login_mvvm/ui/AuthViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.register_login_mvvm.ui
 
+import android.content.Intent
 import android.view.View
 import androidx.lifecycle.ViewModel
 
 class AuthViewModel : ViewModel() {
     var  email : String? = null
     var password : String? = null
+    var username: String? = null
+    var cpassword: String? = null
 
     var authListener : AuthListener? = null
 
@@ -16,5 +19,34 @@ class AuthViewModel : ViewModel() {
             return
         }
         authListener?.onSuccess("welcome home")
+    }
+
+    fun onRegisterClickButton(view : View){
+        authListener?.onStarted("register started")
+        if (email.isNullOrEmpty()){
+            authListener?.onFailure("please enter email address")
+            return
+        }
+
+        if (username.isNullOrEmpty()){
+            authListener?.onFailure("please enter username")
+            return
+        }
+        if (password.isNullOrEmpty()){
+            authListener?.onFailure("enter password")
+            return
+        }
+        if (password != cpassword){
+            authListener?.onFailure("password doesn'match")
+            return
+        }
+
+        authListener?.onSuccess("register successful")
+    }
+
+    fun regLink(view: View){
+        Intent(view.context,RegisterActivity::class.java).also {
+            view.context.startActivity(it)
+        }
     }
 }
--- app/src/main/java/com/example/register_login_mvvm/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/register_login_mvvm/ui/RegisterActivity.kt
@@ -1,13 +1,36 @@
 package com.example.register_login_mvvm.ui
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.example.register_login_mvvm.R
+import com.example.register_login_mvvm.databinding.ActivityLoginBinding
+import com.example.register_login_mvvm.databinding.ActivityRegisterBinding
+import com.example.register_login_mvvm.utils.toast
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(),AuthListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        val binding : ActivityRegisterBinding = DataBindingUtil.setContentView(this,R.layout.activity_register)
+        val viewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
+
+        binding.viewmodel = viewModel
+        viewModel.authListener = this
     }
+
+    override fun onStarted(message: String) {
+        toast(message)
+      }
+
+    override fun onSuccess(message: String) {
+        toast(message)
+      }
+
+    override fun onFailure(message: String) {
+        toast(message)
+     }
 }
