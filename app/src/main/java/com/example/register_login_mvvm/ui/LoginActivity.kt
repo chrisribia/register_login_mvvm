7b4027dc7b3d30751247d4587ce92d70afa6ede7
chrisribia <chrisribia@gmail.com>
2019-07-23 11:04:59 +0300

    setup login activity

--- app/src/main/java/com/example/register_login_mvvm/ui/LoginActivity.kt
+++ app/src/main/java/com/example/register_login_mvvm/ui/LoginActivity.kt
@@ -1,12 +1,13 @@
-package com.example.register_login_mvvm
+package com.example.register_login_mvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.register_login_mvvm.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
